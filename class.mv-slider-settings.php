a07b95c78b9b84976ab8d9c981a57be22cff259c
João Vitor Neves <jvneves_marq@hotmail.com>
2023-12-08 11:12:56 -0300

    aula 33

--- class.mv-slider-settings.php
+++ class.mv-slider-settings.php
@@ -11,7 +11,7 @@ if(!class_exists('MV_Slider_Settings')){
 
     public function admin_init(){
 
-      register_setting('mv_slider_group', 'mv_slider_options');
+      register_setting('mv_slider_group', 'mv_slider_options' , array($this, 'mv_slider_validate'));
 
       add_settings_section(
         'mv_slider_main_section',
@@ -39,21 +39,34 @@ if(!class_exists('MV_Slider_Settings')){
         'Slider Title',
         array($this, 'mv_slider_title_callback'),
         'mv_slider_page2',
-        'mv_slider_second_section'
+        'mv_slider_second_section',
+        array(
+          'label_for' => 'mv_slider_title'
+        )
       );
       add_settings_field(
         'mv_slider_bullets',
         'Display Bullets',
         array($this, 'mv_slider_bullets_callback'),
         'mv_slider_page2',
-        'mv_slider_second_section'
+        'mv_slider_second_section',
+        array(
+          'label_for' => 'mv_slider_bullets'
+        )
       );
       add_settings_field(
         'mv_slider_style',
         'Slider Style',
         array($this, 'mv_slider_style_callback'),
         'mv_slider_page2',
-        'mv_slider_second_section'
+        'mv_slider_second_section',
+        array(
+          'items'=>array(
+            'style-1',
+            'style-2'
+          ),
+          'label_for' => 'mv_slider_style'
+        )
       );
     }
 
@@ -63,7 +76,7 @@ if(!class_exists('MV_Slider_Settings')){
       <?php
     }
 
-    public function mv_slider_title_callback(){
+    public function mv_slider_title_callback($args){
       ?>
         <input 
           type="text" 
@@ -74,7 +87,7 @@ if(!class_exists('MV_Slider_Settings')){
       <?php
     }
 
-    public function mv_slider_bullets_callback(){
+    public function mv_slider_bullets_callback($args){
       ?>
         <input 
           type="checkbox" 
@@ -89,22 +102,50 @@ if(!class_exists('MV_Slider_Settings')){
         <label for="mv_slider_bullets">Wheter to display bullets or not</label>
       <?php
     }
-    public function mv_slider_style_callback(){
+    public function mv_slider_style_callback($args){
       ?>
       <select 
           id="mv_slider_style" 
           name="mv_slider_options[mv_slider_style]">
-          <option value="style-1" 
-              <?php isset( self::$options['mv_slider_style'] ) ? selected( 'style-1', self::$options['mv_slider_style'], true ) : ''; ?>>
-              Style-1
+          <?php 
+          foreach($args['items'] as $item):
+          ?>
+          <option value="<?php echo esc_attr($item);?>"
+            <?php 
+            isset(self::$options['mv_slider_style']) ? selected($item,self::$options['mv_slider_style'],true):'';
+            ?>
+          >
+            <?php echo esc_html(ucfirst($item)); ?>
           </option>
-          <option value="style-2" 
-              <?php isset( self::$options['mv_slider_style'] ) ? selected( 'style-2', self::$options['mv_slider_style'], true ) : ''; ?>>
-              Style-2
-          </option>
+          <?php endforeach; ?>
       </select>
       <?php
   }
+  public function mv_slider_validate( $input ){
+    $new_input = array();
+    foreach ( $input as $key => $value){
+      switch ($key){
+        case 'mv_slider_title':
+          if(empty($value)){
+            $value = 'Please, type some text';
+          }
+          $new_input[$key] = sanitize_text_field($value);
+          break;
+        case 'mv_slider_url':
+          $new_input[$key] = esc_url_raw($value);
+          break;
+        case 'mv_slider_int':
+          $new_input[$key] = absint($value);
+          break;
+        default:
+          $new_input[$key] = sanitize_text_field($value);
+          break;
+      }
+      
+    }
+    return $new_input;
+  }
+
   }
 
 }
